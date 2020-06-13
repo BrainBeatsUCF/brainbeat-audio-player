a42a80c26bfcd65d5ec9dadb17b79739e39b0560
Hung Nguyen <hn9581@gmail.com>
2020-06-12 23:08:02 -0400

    Changed names

--- src/index.tsx
+++ src/index.tsx
@@ -51,7 +51,7 @@ const useStyles = makeStyles(() => ({
   }
 }));
 
-const BrainBeatAudioPlayer: React.FC<AudioPlayerProps> = ({...props}) => {
+const BrainBeatsAudioPlayer: React.FC<AudioPlayerProps> = ({...props}) => {
   const classes = useStyles();
   const audioLength = props.audioObjectArray.length;
   const [indexPlaying, setIndexPlaying] = React.useState(0);
@@ -101,5 +101,5 @@ const BrainBeatAudioPlayer: React.FC<AudioPlayerProps> = ({...props}) => {
   );
 };
 
-export default BrainBeatAudioPlayer;
+export default BrainBeatsAudioPlayer;
 
