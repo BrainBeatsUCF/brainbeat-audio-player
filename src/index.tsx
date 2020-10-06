fbc4dc26278670219473334eced3aa64d40d3783
Gabriel Lara <gabriellara@Gabriels-MacBook-Pro.local>
2020-10-06 11:31:15 -0400

    Added currentAudioPlaying prop and callback.

--- src/index.tsx
+++ src/index.tsx
@@ -1,105 +1,116 @@
-import * as React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AudioPlayer from 'react-h5-audio-player';
-import 'react-h5-audio-player/lib/styles.css';
+import React, { useEffect } from 'react';
+import AudioPlayer from "react-h5-audio-player";
+import 'react-h5-audio-player/lib/styles.css'
+import { makeStyles } from "@material-ui/core/styles";
+
 
 interface AudioObject {
-  title: string,
-  authorName: string,
-  imageUrl: string,
-  audioUrl: string
+    title: string,
+    authorName: string,
+    imageUrl: string,
+    audioUrl: string,
+    audioId: string,
 }
 
-interface AudioPlayerProps { 
-  audioObjectArray: AudioObject[],
-  style?: React.CSSProperties
+interface AudioPlayerProps {
+    audioObjectArray: AudioObject[],
+    style?: React.CSSProperties,
+    currentAudioPlaying?: any
 }
 
 const useStyles = makeStyles(() => ({
-  pictureContainer: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  pictureBorder: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 120,
-    height: 110,
-    width: 110,
-    border: 'solid rgb(208, 53, 30) 1px',
-  },
-  picture: {
-    height: 100,
-    width: 100,
-    borderRadius: '50%'
-  },
-  audioPictureContainer: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 80,
-    width: 80,
-    border: 'solid rgb(208, 53, 30) 1px',
-    borderRadius: '130px',
-  },
-  audioPicture: {
-    height: 60,
-    width: 60,
-    borderRadius: '50%'
-  }
-}));
-
-const BrainBeatsAudioPlayer: React.FC<AudioPlayerProps> = ({...props}) => {
-  const classes = useStyles();
-  const audioLength = props.audioObjectArray.length;
-  const [indexPlaying, setIndexPlaying] = React.useState(0);
-
-  const playForward = () => {
-    let newIndex: number;
-    if (indexPlaying === audioLength - 1) {
-      newIndex = 0;
-    } else {
-      newIndex = indexPlaying + 1;
+    pictureContainer: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    pictureBorder: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 120,
+      height: 110,
+      width: 110,
+      border: 'solid rgb(208, 53, 30) 1px',
+    },
+    picture: {
+      height: 100,
+      width: 100,
+      borderRadius: '50%'
+    },
+    audioPictureContainer: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: 80,
+      width: 80,
+      border: 'solid rgb(208, 53, 30) 1px',
+      borderRadius: '130px',
+    },
+    audioPicture: {
+      height: 60,
+      width: 60,
+      borderRadius: '50%'
     }
-    setIndexPlaying(newIndex);
+  }));
+
+  const callback = (currentAudioPlaying: any, indexPlaying: number):void => {
+   try{
+    currentAudioPlaying(indexPlaying);
+   }
+   catch(e){
+      console.error(e);
+   }
   }
 
-
-  const playBackward = () => {
-    let newIndex: number;
-    if (indexPlaying === 0) {
-      newIndex = audioLength - 1;
-    } else {
-      newIndex = indexPlaying - 1;
+  const BrainBeatsAudioPlayer: React.FC<AudioPlayerProps> = ({...props}) => {
+    const classes = useStyles();
+    const audioLength = props.audioObjectArray.length;
+    const [indexPlaying, setIndexPlaying] = React.useState(0);
+  
+    const playForward = () => {
+      let newIndex: number;
+      if (indexPlaying === audioLength - 1) {
+        newIndex = 0;
+      } else {
+        newIndex = indexPlaying + 1;
+      }
+      setIndexPlaying(newIndex);
+      return newIndex;
     }
-    setIndexPlaying(newIndex);
-  }
-
-  return (
-    <div>
-      <div style={{ display: 'flex', alignContent: 'center', justifyContent: 'center'}}>
-        <div className={classes.audioPictureContainer}>
-          <img className={classes.audioPicture} alt='' src={props.audioObjectArray[indexPlaying].imageUrl}></img>
+  
+    const playBackward = () => {
+      let newIndex: number;
+      if (indexPlaying === 0) {
+        newIndex = audioLength - 1;
+      } else {
+        newIndex = indexPlaying - 1;
+      }
+      setIndexPlaying(newIndex);
+    }
+  
+    return (
+      <div>
+        <div style={{ display: 'flex', alignContent: 'center', justifyContent: 'center'}}>
+          <div className={classes.audioPictureContainer}>
+            <img className={classes.audioPicture} alt='' src={props.audioObjectArray[indexPlaying].imageUrl}></img>
+          </div>
         </div>
+        
+        <AudioPlayer
+          style={props.style}
+          onClickPrevious={playBackward}
+          onClickNext={playForward}
+          src={props.audioObjectArray[indexPlaying].audioUrl}
+          onPlay={e => {callback(props.currentAudioPlaying, indexPlaying)}}
+          onEnded={playForward}
+          header={props.audioObjectArray[indexPlaying].title + ` by ` + props.audioObjectArray[indexPlaying].authorName}
+          showSkipControls
+          showJumpControls = {false}
+          autoPlay
+        />
       </div>
-      
-      <AudioPlayer
-        style={props.style}
-        onClickPrevious={playBackward}
-        onClickNext={playForward}
-        src={props.audioObjectArray[indexPlaying].audioUrl}
-        onPlay={e => console.log("onPlay")}
-        onEnded={playForward}
-        header={props.audioObjectArray[indexPlaying].title + ` by ` + props.audioObjectArray[indexPlaying].authorName}
-        showSkipControls
-        showJumpControls = {false}
-        autoPlay
-      />
-    </div>
-  );
-};
-
-export default BrainBeatsAudioPlayer;
-
+    );
+  };
+  
+  export default BrainBeatsAudioPlayer;
